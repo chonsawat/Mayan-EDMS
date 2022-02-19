c9c8a60ef43b94bf160095208e9ab4993b943348
Chonsawat <chonsawat.nakanam@kkumail.com>
2022-02-19 22:27:02 +0700

    Clear files & Start Install Dev books

--- mayan/apps/permissions/forms.py
+++
@@ -1,9 +0,0 @@
-from mayan.apps.views.forms import DetailForm
-
-from .models import StoredPermission
-
-
-class StoredPermissionDetailForm(DetailForm):
-    class Meta:
-        fields = ('namespace', 'name')
-        model = StoredPermission
--- mayan/apps/user_management/dashboards.py
+++
@@ -1,5 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-
-from mayan.apps.dashboards.classes import Dashboard
-
-dashboard_user = Dashboard(name='user', label=_('User dashboard'))
